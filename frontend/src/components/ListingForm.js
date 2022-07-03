004e8ef9353b068227e1c89987265c05a2c1e1d0
Xarma123 <92310696+Xarma123@users.noreply.github.com>
2022-07-03 11:02:28 +0530

    sass

---
+++ frontend/src/components/ListingForm.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Oval } from "react-loader-spinner";
+import PropTypes from "prop-types";
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,7 +1,50 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-function home() {
-  return <div>home</div>;
+import ListingForm from "../components/ListingForm";
+import Listings from "../components/Listings";
+import Pagination from "../components/Pagination";
+
+function Home() {
+  const [listings, setListings] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listingsPerPage, setListingsPerPage] = useState(3);
+  const [active, setActive] = useState(1);
+
+  const indexOfLastListing = currentPage * listingsPerPage;
+  const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+  const currentListings = listings.slice(
+    indexOfFirstListing,
+    indexOfLastListing
+  );
+
+  const visitPage = (page) => {
+    setCurrentPage(page);
+    setActive(page);
+  };
+
+  const previous_number = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+      setActive(currentPage - 1);
+    }
+  };
+
+  const next_number = () => {
+    if (currentPage !== Math.ceil(listings.length / 3)) {
+      setCurrentPage(currentPage + 1);
+      setActive(currentPage + 1);
+    }
+  };
+
+  return (
+    <main className="home">
+      <Helmet>
+        <title>Realest Estate - Home</title>
+        <meta name="description" content="Realest Estate Home Page" />
+      </Helmet>
+      
+    </main>
+  );
 }
 
-export default home;
+export default Home;
